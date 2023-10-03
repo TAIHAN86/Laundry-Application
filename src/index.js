abffbf3af782494c377035660b263481a9eb84ef
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-10-03 22:51:08 +0530

    New folders aadded to src folder

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Greeting,{Greeting1,Greeting2}from './Greeting.js';
-
-
+import Home from './views/Home/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-
- 
-root.render(
-  <>
-  <div className='main-container'>
-  <Greeting name='PRAJAKTA'/>
-  <Greeting1 name='MANASII'/>
-  <Greeting2 name='MRUNAL'/>
-  </div>
-  </>
- 
-);
+root.render(<Home />);
