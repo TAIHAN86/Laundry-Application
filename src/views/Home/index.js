1236a884b38a9275633a1af124ea78171e16dd4b
AMAN KHAN <124271834+TAIHAN86@users.noreply.github.com>
2023-11-19 17:07:54 +0530

    Added css

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./Home.css"
 
-function Home () {
+function Home() {
   return (
     <div>
-        <h1 className='app-title'>contact app</h1>
+      <h1 className='app-title'>▶️contact app</h1>
+
+
+      <div classname="app-body">
+
+        <div className='contact-container'>
+          <h2> Display contact</h2>
+        </div>
+
+        <div className='add-contact-container'>
+          <h2>Add contact</h2>
+        </div>
+      </div>
     </div>
   )
 }
